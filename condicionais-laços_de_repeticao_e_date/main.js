e3e899a0d0971335fb8e086651676736f503d3e3
deivsonsoares <deivsonds.ds@gmail.com>
2021-11-24 07:23:38 -0300

    dado sequencia na aula condicionais

---
+++ condicionais-laços_de_repeticao_e_date/main.js
@@ -0,0 +1,19 @@
+//laços de repeticao
+var count = 0;
+while (count <= 5 ){
+    console.log(count);
+    alert(count);
+    count ++;
+};
+
+
+//condicional
+/*
+var idade = prompt("Qual a sua idade?")
+var idade = 18;
+if (idade >= 18){
+    alert("maior de idade!");
+}else{
+    alert("menor de idade!");
+};
+*/
