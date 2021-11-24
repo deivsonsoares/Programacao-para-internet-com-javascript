614611aa74e91bb6da0e7526e973614558c6e686
deivsonsoares <deivsonds.ds@gmail.com>
2021-11-24 07:03:18 -0300

    dado sequencia na aula

---
+++ array-dicionario/main.js
@@ -0,0 +1,26 @@
+var lista = ['maça',"pera", 'laranja'];
+
+var fruta = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(fruta);
+alert(fruta[1].nome);
+//acrescenta item
+//lista.push("uva");
+
+//retira item
+//lista.pop('laranja');
+
+//tamanho da lista
+//console.log(lista.length);
+
+//imprime lista de forma inversa
+//console.log(lista.reverse);
+
+//imprimi por indice
+//alert(lista[2]);
+
+//impressao
+//console.log(lista[0]);
+//console.log(lista.toString(1));
+
+//separa informacoes
+//console.log(lista.join(" - "))
---
+++ introducao-javascript/js/main.js
@@ -0,0 +1,9 @@
+var nome = " Deivson Soares ";
+var idade = 29;
+var idade2 = 10;
+var frase = " Japão é o melhor time do mundo ";
+//alert ( nome + " tem " + idade + " anos!");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade2 + idade);
+console.log(frase.replace("Japão", "Brasil"));
